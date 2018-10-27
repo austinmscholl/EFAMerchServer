d4993dbf53852258c95bd4a66c1699a0fdd8b082
Drew Blincoe <Drew@Drews-MacBook-Pro.local>
2018-10-27 16:35:11 -0500

    index

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-require('dotenv').config();
-
-var user = require('./controllers/usercontroller');
+let user = require('./controllers/usercontroller');
+let item = require('./controllers/itemcontroller')
 
 const express = require('express');
 const app = express();
-<<<<<<< HEAD
-let Item = require('./controllers/itemcontroller');
-let user = require('./controllers/usercontroller');
-let sequelize = require('./db')
-let bodyParser = require('body-parser')
-
-
-sequelize.sync()
-app.use(require('./middleware/headers'));
-app.use(bodyParser.json());
-
-app.use('/', user);
-app.use('/item', Item)
-=======
 var sequelize = require('./db');
 var bodyParser = require('body-parser');
 
 sequelize.sync();
 
 app.use(bodyParser.json());
->>>>>>> development
 
 app.use(require('./middleware/headers'))
 
 app.use('/', user);
+app.use('/item', item)
 
 app.listen(5000,() => console.log('app is listening on port 5000...bitches'));
