bf4e02924f7f007386515def2cd821c5708a16a6
Drew Blincoe <Drew@Drews-MacBook-Pro.local>
2018-10-27 16:26:26 -0500

    updated index

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 let Item = require('./controllers/itemcontroller');
+let user = require('./controllers/usercontroller');
 let sequelize = require('./db')
 let bodyParser = require('body-parser')
 
-sequelize.sync()
 
+sequelize.sync()
+app.use(require('./middleware/headers'));
+app.use(bodyParser.json());
+
+app.use('/', user);
 app.use('/item', Item)
+
+
 
 app.listen(5000,() => console.log('app is listening on port 5000...bitches'));
