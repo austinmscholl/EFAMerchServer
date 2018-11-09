4382a39e9ee60048972dbd32f9cbc966d02eaebb
austinmscholl <asprof34@gmail.com>
2018-11-09 15:42:53 -0500

    changed

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,4 +1,4 @@
-module.exports = function(req, res, next){
+module.exports =(req, res, next)=>{
 
     res.header('access-control-allow-origin', '*');
     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE');
