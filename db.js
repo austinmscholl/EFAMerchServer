bda4be875b2ea0c4775d98facf77bde98feb4ad2
Drew Blincoe <Drew@Drews-MacBook-Pro.local>
2018-11-14 20:07:03 -0500

    hey

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config()
 
 const sequelize = new Sequelize(
-    process.env.DATABASE_URL
+    process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/efaMerchServer`
     , {
     dialect: 'postgres'
 });
